8c4a71e82820c00bb4bea3c3f24a28354b6690ae
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-04-18 09:15:59 +0700

    service helper

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,8 +1,15 @@
-// import axios from "axios"
+import axios from "axios"
 
-// const Service = {
-//     fetchService: async (url: any) => {
-//         const response = await axios(url);
-//         return new
-//     }
-// }
+export const Service = {
+    fetchService: async (url: any) => {
+        try {
+            const response = await axios(url);
+            return new Promise((resolve, reject) => {
+                if (response.status === 200) resolve(response)
+                reject(response)
+            })
+        } catch (error) {
+            throw error;
+        }
+    }
+}
