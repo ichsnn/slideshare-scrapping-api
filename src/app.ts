62e1456e357b55e94049e2db29527f1019eedeb9
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-04-18 14:53:17 +0700

    add logger

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 import { error } from "./error";
-
 import routes from "./routes";
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(cors());
-
-app.use(routes)
-
-app.use(error)
+app.use(routes);
+app.use(error);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
