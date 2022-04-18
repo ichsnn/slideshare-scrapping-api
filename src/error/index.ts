639cfc61b85570b9d3d319a7ccf81d176f6f75fc
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-04-18 15:06:48 +0700

    error response

--- src/error/index.ts
+++ src/error/index.ts
@@ -2,5 +2,5 @@ import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 
 export const error = (err : any, req: Request, res: Response, next: NextFunction) => {
     res.status(500)
-    res.send('Internal Server Error</br>' + '<pre>'+err.stack+'</pre>')
+    res.send({status: false, message: err.message, stack: err.stack})
 }
