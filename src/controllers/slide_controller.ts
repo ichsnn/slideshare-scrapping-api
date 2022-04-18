14a5789111aa3c6e688081fb95255e69a7ac2c38
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-04-18 13:39:56 +0700

    getSlideImg

--- src/controllers/slide_controller.ts
+++ src/controllers/slide_controller.ts
@@ -1,12 +1,45 @@
 import { NextFunction, Request, Response } from "express";
+import cheerio from "cheerio";
 import { Service } from "../helper";
 
+const getSlidesImg: any = (req: Request, res: Response, response: any) => {
+    try {
+        const $ = cheerio.load(response.data);
+        const slideContainer = $('#slide-container');
+        const slideImgColl: any[] = []
+        const lazyColl: any[] = [];
+
+        slideContainer.find('.slide').each((i, e) => {
+            let src;
+            if (src = $(e).find('img').attr('srcset')) {
+                slideImgColl.push(src)
+            } else {
+                lazyColl.push($(e))
+            }
+        })
+
+        lazyColl.map((v: cheerio.Element, j) => {
+            let doc = cheerio.parseHTML($(v).text());
+            $(doc).each((i, e) => {
+                let src;
+                if (src = $(e).attr('srcset')) {
+                    slideImgColl.push(src);
+                }
+            })
+        })
+        return slideImgColl;
+    } catch (error) {
+        throw error;
+    }
+}
+
 const SlidesController = {
-    slidesImg: async (req : Request, res : Response, next : NextFunction) => {
+    slidesImg: async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const url : any = req.query.url;
-            const response : any = await Service.fetchService(url);
-            res.send('OK!');
+            const url: any = req.query.url;
+            const response: any = await Service.fetchService(url);
+            const imgCol = getSlidesImg(req, res, response);
+            res.send(imgCol);
         } catch (error) {
             next(error)
         }
