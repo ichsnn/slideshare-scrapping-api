af1f199c3d9df2a474279a4b861aa6b6c8daa5e0
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-04-20 16:50:30 +0700

    fix sandbox problem

--- src/utils/pdfGenerator.ts
+++ src/utils/pdfGenerator.ts
@@ -1,7 +1,9 @@
 import puppeteer from "puppeteer";
 
 const pdfGenerator = async (html : any) => {
-    const browser  = await puppeteer.launch();
+    const browser  = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.setContent(html);
     await page.pdf({path: './files/slideshare.pdf', height: "576px", width: "1024px", preferCSSPageSize: true});
