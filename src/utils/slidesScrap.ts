f7e46e412486490cfa607da9da1abd81c9d2acc5
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-05-10 08:55:24 +0700

    make faster

--- src/utils/slidesScrap.ts
+++ src/utils/slidesScrap.ts
@@ -8,30 +8,29 @@ const slidesScrap = {
         try {
             const $ = cheerio.load(response.data);
             const slideContainer = $('#slide-container');
-            const slideImgColl: any[] = [] // doesn't contain noscript tag
+            const imgColls: any[] = []; // for objects
 
             // search slide contain img || srcset
             slideContainer.find('.slide').each((i, e) => {
+                let obj;
                 let srcset;
                 if (srcset = $(e).find('img').attr('srcset')) {
-                    slideImgColl.push(srcset)
+                    obj = srcset;
                 } else {
                     // Convert image contain noscript or doesnt include img tag on first node
                     let doc = cheerio.parseHTML($(e).text());
                     $(doc).each((i, v) => {
                         if (srcset = $(v).attr('srcset')) {
-                            slideImgColl.push(srcset)
+                            obj = srcset;
                         }
                     })
                 }
+                createArrayOfObject(obj, i);
             })
 
-            const imgColls: any[] = []; // for objects
-
-            // Convert array to Object
-            slideImgColl.map((v: string, i) => {
+            function createArrayOfObject(v : any, i : number) {
                 const src = v.split(', ')
-                const x = src.map((v) => {
+                const x = src.map((v : any) => {
                     const results = v.split(' ');
                     return {
                         resolution: results[1],
@@ -40,7 +39,8 @@ const slidesScrap = {
 
                 })
                 imgColls.push({ 'slide': i + 1, image: x })
-            })
+            }
+
 
             // return array of object
             return imgColls;
