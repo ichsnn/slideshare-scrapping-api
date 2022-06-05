f3af8d70a22ef882bfd366f8e26a061536b6bd02
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-06-05 07:23:13 +0700

    fix cors error

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,16 @@ import routes from "./routes";
 
 const app = express();
 
-const whitelist = ['https://ichsnn.github.io/*', 'http://localhost:5500/*', 'http://127.0.0.1:5500/*']
+const whitelist = ['https://ichsnn.github.io', 'http://localhost:5500', 'http://127.0.0.1:5500']
 
-const corsOptions : any = {
-  origin: function (origin : any, callback : any) {
-    if(whitelist.indexOf(origin) !== -1) {
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    console.log(origin)
+    if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
+    }
+    else {
+      callback(new Error('Not allowed by CORS'))
     }
   }
 }
@@ -20,7 +24,7 @@ const corsOptions : any = {
 const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(morgan('dev'));
-app.use(cors({origin: corsOptions}));
+app.use(cors(corsOptions));
 app.use(routes);
 app.use(error);
 
